13aa7b83c68176f2533af43f174804fdc2c08ca4
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-10 21:35:50 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ * main 
+ *
+ * Return: 0
+ */
 int main (void)
 {
-  puts(Programming is like buildin a multilingual puzzle)
-  return 0;
+    puts("\"Programming is like buildin a multilingual puzzle");
+    return (0);
 }
