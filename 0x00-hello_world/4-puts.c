6c8e200d9a573d0285a5b901958a8f1fca0874f8
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-10 22:33:52 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - entry point
- * Return: always 0 (succes)
+ *
+ * Return: always 0 (success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
