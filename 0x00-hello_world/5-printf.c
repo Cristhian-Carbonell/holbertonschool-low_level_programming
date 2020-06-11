20b57f70625f1e6f0b5bc274dfae68a4c8e02222
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-11 00:50:51 +0000

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point 
  *
- *
- *
+ * Using printf to print formatted string
+ * Return: 0 (success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
