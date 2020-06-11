bc91aa4ecdd84f90e66d564dd59d5899e6056af0
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-11 04:33:48 +0000

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - prints message to stderr
  *
- * return: 1
+ * return: 0
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
 	      59);
 	return (1);
 }
