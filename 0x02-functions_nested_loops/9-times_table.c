b7d546b424ecbe77aced8f0e9581fcf236173582
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-16 16:05:29 +0000

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			int a, b;
+
+			a = (x * y) / 10;
+			b = (x * y) % 10;
+			if (x * y > 9)
+				_putchar(a + '0');
+			_putchar(b + '0');
+			if (y == 9)
+				break;
+			_putchar(',');
+			_putchar(' ');
+			if ((x * (y + 1)) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
