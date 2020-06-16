ec1b253b45d7772e89926c84227af41d2679cc68
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-16 16:25:02 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers
+ * @arg1: 1st number to be summed
+ * @arg2: 2nd number to be summed
+ *
+ * Return: sum of arg1 and arg2
+ */
+int add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
