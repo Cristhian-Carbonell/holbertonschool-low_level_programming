7244da8d0f5e87f37445ad1d3645f5e7e2a3fad3
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-23 19:15:21 +0000

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * swap_int - prints two values that are exchanged
+ * @a: variable that prints the value 42
+ * @b: variable that prints the value 98
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
