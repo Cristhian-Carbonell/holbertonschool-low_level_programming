c93cf111918508706604d90034f74d11c01568ca
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-24 04:10:38 +0000

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copy string from source to destination including
+ * terminating null byte
+ * @dest: destination for string to be copied
+ * @src: string to be copied
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int b;
+
+	for (b = 0; (dest[b] = src[b]); b++)
+	{
+		;
+	}
+	return (dest);
+}
