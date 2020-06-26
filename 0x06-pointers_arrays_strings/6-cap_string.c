68c50a9862a54f288c6b5c52a8878435b826478e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-26 04:00:00 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all word of a string
+ * @str: character string
+ *
+ * Return: string with each word capitalized
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+			}
+			else if (str[i - 1] == 32||'\t'||'\n'||','||';'||'.'||'!'||'?'
+				 ||'"'||'('||')'||'{'||'}')
+				str[i] -= 32;
+		}
+	}
+	return (str);
+}
