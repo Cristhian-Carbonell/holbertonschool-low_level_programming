e62077de6d5d18bccf6bf2c454f0a3f59b0c5033
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-26 23:58:26 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,24 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (i == 0)
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[0] >= 'a' && str[0] <= 'z')
+			{
+				str[0] -= 32;
+			}
+			if (str[i] >= 'a' && str[i] <= 'z' &&
+			    (str[i - 1] == ',' || str[i - 1] == ';' ||
+				 str[i - 1] == '.' || str[i - 1] == '!' ||
+				 str[i - 1] == '?' || str[i - 1] == '"' ||
+				 str[i - 1] == '\n' || str[i - 1] == '(' ||
+				 str[i - 1] == ')' || str[i - 1] == '{' ||
+			     str[i - 1] == '}' || str[i - 1] == 32 || '\t'))
 			{
 				str[i] -= 32;
 			}
-			else if (str[i - 1] == 32 || '\t' || '\n' || ',' || ';'
-				 || '.' || '!' || '?' || '"' || '(' || ')' ||
-				 '{' || '}')
-			{
-				str[i] -= 32;
-			}
-		}
+			i++;
 	}
 	return (str);
 }
