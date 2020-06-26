912c90537e40dd014f5f34852fd28e16776a0d6e
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-26 02:33:29 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strncat -
+ * @dest:
+ * @src:
+ * @n:
+ *
+ * Return:
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, a;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		;
+	}
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[i + a] = src[a];
+	}
+
+	return (dest);
+}
