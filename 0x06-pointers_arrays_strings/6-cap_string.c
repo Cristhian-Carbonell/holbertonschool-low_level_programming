4139739f2cacecec36a2a14431950f112025ad59
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-27 00:10:02 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,24 +10,32 @@ char *cap_string(char *str)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
+	if (str[0] >= 'a' && str[0] <= 'z')
 	{
-			if (str[0] >= 'a' && str[0] <= 'z')
-			{
-				str[0] -= 32;
-			}
-			if (str[i] >= 'a' && str[i] <= 'z' &&
-			    (str[i - 1] == ',' || str[i - 1] == ';' ||
-				 str[i - 1] == '.' || str[i - 1] == '!' ||
-				 str[i - 1] == '?' || str[i - 1] == '"' ||
-				 str[i - 1] == '\n' || str[i - 1] == '(' ||
-				 str[i - 1] == ')' || str[i - 1] == '{' ||
-			     str[i - 1] == '}' || str[i - 1] == 32 || '\t'))
-			{
-				str[i] -= 32;
-			}
-			i++;
+		str[0] -= 32;
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		switch (str[i])
+		{
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+		case ' ':
+		case '\n':
+		case '\t':
+			if (str [i + 1] > 'a' && str [i +1] < 'z')
+				{
+					str[i + 1] -= 32;
+				}
+		}
 	}
 	return (str);
 }
