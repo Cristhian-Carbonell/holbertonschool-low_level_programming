67260f179b093e4407e876fc60e8f94beceb9b4c
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-27 00:16:18 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -31,10 +31,11 @@ char *cap_string(char *str)
 		case ' ':
 		case '\n':
 		case '\t':
-			if (str [i + 1] > 'a' && str [i +1] < 'z')
-				{
-					str[i + 1] -= 32;
-				}
+
+			if (str[i + 1] > 'a' && str[i + 1] < 'z')
+			{
+				str[i + 1] -= 32;
+			}
 		}
 	}
 	return (str);
