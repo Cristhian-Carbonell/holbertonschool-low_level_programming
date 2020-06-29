d85237fda78143c33b7f0597c74e986eb6495072
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-29 02:12:35 +0000

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - function that fills memory with a constant byte
  * @s: points bytes to fill in memory
  * @b: fill the first bytes of the memory area
- * @n; constante
+ * @n: constante
  *
  * Return: a pointer to the memory area s
  */
