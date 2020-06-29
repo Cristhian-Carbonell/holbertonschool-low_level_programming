8015e4c177c62eb63ad0ea192e7999d01f9a0a21
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-29 23:49:44 +0000

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: locates the first occurrence in the string
+ * @accept: bytes in teh string
+ *
+ * Return: a pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int b;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[i] == accept[b])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return (s);
+}
