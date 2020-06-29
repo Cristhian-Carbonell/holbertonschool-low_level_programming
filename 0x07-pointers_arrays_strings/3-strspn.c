565957292a17b04baa678fa58c6961e354887ef1
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-06-29 23:11:08 +0000

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strspn - that gets the length of a prefix substring
+ * @s: of the initial segment
+ * @accept: consists entirely of bytes
+ *
+ * Return: the number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int b;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[i] == accept[b])
+			{
+				c++;
+				break;
+			}
+		}
+		if (accept[b] == '\0')
+		{
+			return(c);
+		}
+	}
+	return (c);
+}
