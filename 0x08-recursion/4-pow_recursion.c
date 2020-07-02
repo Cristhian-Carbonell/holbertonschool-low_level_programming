5b9935ee498e936c5b592acf0dd15687f6ce623b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-02 22:59:39 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * _pow_recursion -
- * @x:
- * @y:
+ * _pow_recursion - return the value of x raised to the power of y
+ * @x: integer
+ * @y: integer
  *
- * Return: returns the value of x elevado a la potencia y
+ * Return: if y is lower than 0, the function should return -1
  */
 int _pow_recursion(int x, int y)
 {
