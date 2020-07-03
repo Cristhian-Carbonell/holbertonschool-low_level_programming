37941c6e06c1660c9d26beb8447219a5b8a8415d
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-03 04:10:27 +0000

    Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * primo_noPrimo - auxiliary function
+ * @n: integer
+ * @y: integer
+ *
+ * Return: 1 is prime, 0 not
+ */
+int primo_noPrimo(int n, int y)
+{
+	if (n == y)
+	{
+		return (1);
+	}
+	if (n % y == 0)
+	{
+		return (0);
+	}
+	if (n == 1 || n < 0)
+	{
+		return (0);
+	}
+	return (primo_noPrimo(n, y + 1));
+}
+
+/**
+ * is_palindrome - check if the input integer is a prime number
+ * @s: integer
+ *
+ * Return: 1 if the input iteger is a prime number, 0 not
+ */
+int is_prime_number(int n)
+{
+	return (primo_noPrimo(n, 2));
+}
