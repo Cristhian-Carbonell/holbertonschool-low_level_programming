f3e60c48a3ef4fe8f671116fc6563d1d3d786cf3
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-03 02:18:08 +0000

    Your subconscious is looking for the dreame

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * square_root - auxiliary function
+ * @i: integer
+ * @j: integer
+ *
+ * Return: root, -1 not
+ */
+int calculate_raiz(int x, int y)
+{
+	int raiz = y * y;
+
+	if (raiz > x)
+	{
+		return (-1);
+	}
+	if (raiz == x)
+	{
+		return (y);
+	}
+
+	return (calculate_raiz(x, y + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ *
+ * Return: root, -1 not
+ */
+int _sqrt_recursion(int n)
+{
+	return (calculate_raiz(n, 1));
+}
