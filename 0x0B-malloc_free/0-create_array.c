b42ce69cb8eacbcaf72fe0a687f1fc26b87e9596
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-07 22:46:39 +0000

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -21,10 +21,10 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
-	}
-	if (ptr == NULL)
-	{
-		return (ptr);
+		if (ptr == NULL)
+		{
+			return (ptr);
+		}
 	}
 	return (ptr);
 }
