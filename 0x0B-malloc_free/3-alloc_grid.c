50c9103497ec267c4e2be4cd9f2c43ae3b9ea536
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-09 02:31:26 +0000

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,12 +23,12 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		*(cuadri + i) = malloc(width * sizeof(int));
-		if (*(cuadri + i) == NULL)
+		cuadri[i] = malloc(width * sizeof(int));
+		if (cuadri[i] == NULL)
 		{
 			for (j = 0; j < i; j++)
 			{
-				free(*(cuadri + i));
+				free(cuadri[i]);
 			}
 			free(cuadri);
 			return (NULL);
