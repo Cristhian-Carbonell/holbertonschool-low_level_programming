e3de372edfc03b9f293e5346cf1489c985fa9859
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-09 03:40:46 +0000

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * argstostr - that concatenates all the arguments of your program
+ * @ac: integer
+ * @av: doble pointer character
+ *
+ * Return: a pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, longitud;
+	char *ptr;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	longitud = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			longitud++;
+		}
+	}
+	ptr = malloc((longitud + ac)* sizeof(char));
+	longitud = 0;
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			ptr[longitud] = av[i][j];
+			longitud++;
+		}
+		ptr[longitud] = '\n';
+		longitud++;
+	}
+	ptr[longitud] = '\0';
+	return (ptr);
+
+}
