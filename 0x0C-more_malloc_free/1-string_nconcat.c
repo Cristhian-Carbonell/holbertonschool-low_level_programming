61777545fd28acacdbfd2042706b033387b6157c
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-10 03:58:26 +0000

    string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+
+/**
+ * string_nconcat - that concatenates two stings
+ * @s1: pointer string
+ * @s2: pointer string
+ * @n: size
+ *
+ * Return: pointer
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int i = 0, j = 0, a = 0, b = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+	j++;
+	if (n > j)
+		ptr = malloc((i + j) * sizeof(char));
+	else
+		ptr = malloc((i + n + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	while (a < i)
+	{
+		ptr[a] = s1[a];
+		a++;
+	}
+	while (b < j && b < n)
+	{
+		ptr[a] = s2[b];
+		b++;
+		a++;
+	}
+	ptr[a] = '\0';
+	return (ptr);
+}
