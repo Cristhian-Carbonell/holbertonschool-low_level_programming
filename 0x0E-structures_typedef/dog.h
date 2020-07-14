de08c12becc6a41d7f0c97640f56513d00146bcd
Retr-0 <1854@holbertonschool.com>
2020-07-14 02:15:24 +0000

    Django

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,15 @@
 #define DOG_H
 
 int _putchar(char c);
+
+/**
+ * struct dog - characteristic of a dog
+ * @name: dog's name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: know the characteristics of a dog
+ */
 struct dog
 {
 	char *name;
