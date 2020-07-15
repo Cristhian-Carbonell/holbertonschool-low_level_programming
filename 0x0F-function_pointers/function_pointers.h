f3644a51c35796e2072e4df9c241e1be454592d8
Retr-0 <1854@holbertonschool.com>
2020-07-15 22:00:50 +0000

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,6 @@
 #define FUNCTION_POINTERS
 
 void print_name(char *name, void (*f) (char *));
-/*void array_iterator(int *array, size_t size, void (*action)(int));*/
 int int_index(int *array, int size, int (*cmp)(int));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
