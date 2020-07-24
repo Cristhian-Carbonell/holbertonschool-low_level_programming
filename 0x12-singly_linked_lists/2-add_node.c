ebb916076fb0b4937abbf901a28d801210f0f7be
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-24 02:47:14 +0000

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,7 +3,7 @@
 /**
  * add_node - function that adds a new node at the beginning of a list_t list
  * @head: memory location
- * @char: string elements
+ * @str: string elements
  *
  * Return: the address of the new element, or NULL if if falied
  */
@@ -19,7 +19,6 @@ list_t *add_node(list_t **head, const char *str)
 		if (str == NULL)
 			return (NULL);
 	new_node->str = strdup(str);
-
 	new_node->len = i;
 	new_node->next = (*head);
 	(*head) = new_node;
