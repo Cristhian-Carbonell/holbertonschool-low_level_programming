18b7389e4d7e3e61706e0f95cd2f23c4070b88b5
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-24 03:32:19 +0000

    Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,14 +6,5 @@
  */
 void free_list(list_t *head)
 {
-	list_t *current = head;
-	list_t *next;
-
-	while (current != NULL)
-	{
-		next = current->next;
 		free(current);
-		current = next;
-	}
-	head = NULL;
 }
