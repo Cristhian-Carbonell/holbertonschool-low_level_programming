f2b0b8972dfb00c5dd48137b3749571972b670b8
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-24 18:22:16 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * printf_ - function that prints string before the main fuction
+ * __attribute__((constructor)) is used to execute a function the
+ * program starts.
+ */
+void printf_(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
