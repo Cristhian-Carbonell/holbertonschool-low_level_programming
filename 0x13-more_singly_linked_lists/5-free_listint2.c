62dc1778c6fe6e8ac5ccfef2f915a082d9e76416
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-28 20:09:36 +0000

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free-listint2 - fuction that frees a listint_t list
+ * @head: node
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current = 0;
+
+	while (head != NULL)
+	{
+		current = (*head)->next;
+		free_listint2(&(*head)->next);
+		free(*head);
+		*head = current;
+		*head = NULL;
+	}
+}
