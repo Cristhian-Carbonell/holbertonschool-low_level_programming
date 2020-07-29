5444ce44dce40eb90905efdba5411705e96e5432
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-07-29 02:27:59 +0000

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,6 +24,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		next = &(*next)->next;
 		i++;
 	}
+	if (idx == 0)
+	{
+		return (NULL);
+	}
 	new_node->next = *next;
 	*next = new_node;
 	return (new_node);
