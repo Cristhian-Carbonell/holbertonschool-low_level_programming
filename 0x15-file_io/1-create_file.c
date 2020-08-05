8f8405dd787ee4d16afc76cab2d84bdf4178049c
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-08-05 00:44:52 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	if (filename == filename)
+		fd = open(filename, O_TRUNC | 0600);
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
-		return (0);
-	write(fd, text_content, 0);
+		return (-1);
+	if (text_content == NULL)
+		write(fd, text_content, 0);
+	else
+		write(fd, text_content, 5);
+
 	close(fd);
 	return (1);
 }
