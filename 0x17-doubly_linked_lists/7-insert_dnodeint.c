6ce208e80697fa6fb39d695098d8b2f55e8f02c1
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-07 23:36:53 +0000

    Insert list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,39 +11,61 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp;
+	dlistint_t *new_node, *atreversar;
 	unsigned int i;
+	size_t length_list;
 
 	length_list = dlistint_len(*h);
+	atreversar = *h;
 	if (h == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
-	tmp = *h;
+	new_node->n = n;
+	new_node->next = new_node->prev = NULL;
+	if (length_list < idx)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		if (*h != NULL)
+		{
+			new_node->next = *h;
+			(*h)->prev = new_node;
+		}
+		*h = new_node;
+		return (*h);
+	}
 	i = 0;
 	while (i < idx - 1)
 	{
-		if (tmp)
-			tmp = tmp->next;
-		else
-		{
-			free(new);
-			return (NULL);
-		}
+		atreversar = atreversar->next;
 		i++;
 	}
-	if (!tmp->next)
+	new_node->next = atreversar->next;
+	new_node->prev = atreversar;
+	atreversar->next = new_node;
+	if (new_node->next != NULL)
+		new_node->next->prev = new_node;
+	return (new_node);
+}
+
+/**
+ * dlistint_len - returns the number of elements in linked dlistint_t list
+ * @h: pointer  to head
+ *
+ * Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
 	{
-		free(new);
-		return (add_dnodeint_end(h, n));
+		h = h->next;
 	}
-	new->next = tmp->next;
-	new->prev = tmp;
-	tmp->next->prev = new;
-	tmp->next = new;
-	return (new);
+	return (i);
 }
