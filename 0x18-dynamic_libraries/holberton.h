d69575e41bea6b7fbe46cb235cfe6a096cc727db
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-08 02:11:24 +0000

    A library is not a luxury but one of the necessities of lige

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,4 +1,4 @@
-#ifndef HOLBERTON_H
+B#ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <unistd.h>
 
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBERTON_H */
+#endif /* HOLBERTON */
