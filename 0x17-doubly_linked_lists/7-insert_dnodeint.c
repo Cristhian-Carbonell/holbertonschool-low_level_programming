dc6a9c5db0cc3d698a7791655d25837030916f34
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-09-08 00:02:00 +0000

    Insert list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		i++;
 	}
-	if (tmp->next != NULL)
+	if (tmp->next == NULL)
 	{
 		free(new_node);
 		return (add_dnodeint_end(h, n));
