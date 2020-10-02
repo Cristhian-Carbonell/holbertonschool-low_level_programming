18c7595e29ee19841229091ce9cd50fe8d6b815b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-02 23:19:19 +0000

    >>> ht['betty'] = 'holberton'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,19 +10,34 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *element, *tmp;
-	unsigned long int index;
+	hash_node_t *element;
 
 	if (ht == NULL)
 		return (0);
+
 	element = malloc(sizeof(hash_node_t));
 	if (element == NULL)
 		return (0);
+
 	element->key = strdup(key);
 	element->value = strdup(value);
+	insert_element_list(ht, element);
+
+	return (1);
+}
+
+/**
+ * insert_element_list - function inserts our elements in the list
+ * @ht: is the hash table you want to add or update the key/value to
+ * @elemente: insert an elemente into the given index of the array
+ */
+void insert_element_list(hash_table_t *ht, hash_node_t *element)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
 	index = key_index((unsigned char *)element->key, ht->size);
 	tmp = ht->array[index];
-
 	if (ht->array[index] != NULL)
 	{
 		tmp = ht->array[index];
@@ -51,5 +66,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		element->next = NULL;
 		ht->array[index] = element;
 	}
-	return (1);
 }
