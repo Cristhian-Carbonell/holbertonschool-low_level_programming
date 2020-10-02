d2010c6434f4a2eb7cf222e2b22fa43e774a78f6
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-02 21:56:43 +0000

    key->index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,11 +10,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index;
+	unsigned long int index ;
 	unsigned long int i;
 
-	index = 0;
-	for (i = 0; key && key[i]; i++)
+	for (i = 0; key != NULL && key[i] != '\0'; i++)
 		index = (index + key[i] % size);
 	return (index);
 }
