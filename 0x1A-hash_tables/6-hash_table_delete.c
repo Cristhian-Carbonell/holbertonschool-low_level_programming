89450d07445bad7baec1861c7b586ca1609f25fb
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-10-03 02:06:48 +0000

    >>> del ht

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -15,12 +15,12 @@ void hash_table_delete(hash_table_t *ht)
 		if (ht->array[i] != NULL)
 		{
 			while (ht->array[i] != NULL)
-				{
-					free(ht->array[i]->key);
-					free(ht->array[i]->value);
-					free(ht->array[i]);
-					ht->array[i] = ht->array[i]->next;
-				}
+			{
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = ht->array[i]->next;
+			}
 			free(ht->array[i]);
 		}
 	}
