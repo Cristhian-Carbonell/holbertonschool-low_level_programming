710480db4249c69e69681fbe772f9fc409e97c61
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-04 23:36:37 +0000

    Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,37 +13,36 @@ void print_number(int n)
 		n *= -1;
 		_putchar('-');
 	}
-	if (n == 0)
+	if (n < 10)
 		_putchar(n + '0');
-	else
-		if (n < 100)
-		{
-			a = n / 10;
-			b = n % 10;
-			_putchar(a + '0');
-			_putchar(b + '0');
-		}
-		else if (n < 1000)
-		{
-			temp = n / 10;
-			a = temp / 10;
-			b = temp % 10;
-			c = n % 10;
-			_putchar(a + '0');
-			_putchar(b + '0');
-			_putchar(c + '0');
-		}
-		else if (n < 10000)
-		{
-			temp = n / 10;
-			c = temp % 10;
-			temp1 = temp / 10;
-			a = temp1 / 10;
-			b = temp1 % 10;
-			d = n % 10;
-			_putchar(a + '0');
-			_putchar(b + '0');
-			_putchar(c + '0');
-			_putchar(d + '0');
-		}
+	else if (n < 100)
+       	{
+		a = n / 10;
+		b = n % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+	}
+	else if (n < 1000)
+	{
+		temp = n / 10;
+		a = temp / 10;
+		b = temp % 10;
+		c = n % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(c + '0');
+	}
+	else if (n < 10000)
+	{
+		temp = n / 10;
+		c = temp % 10;
+		temp1 = temp / 10;
+		a = temp1 / 10;
+		b = temp1 % 10;
+		d = n % 10;
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar(c + '0');
+		_putchar(d + '0');
+	}
 }
