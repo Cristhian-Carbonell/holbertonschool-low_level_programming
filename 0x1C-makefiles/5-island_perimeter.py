66e9ab02e4a7d8a467be8fcb215a918c8478ea8b
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-08 00:33:57 +0000

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,23 +3,36 @@
 that returns the perimeter of the island described in grid:
 """
 
+
 def island_perimeter(grid):
     """
     that returns the perimeter of the island described in grid:
     """
-    count = 0
-    index1 = 0
-    index2 = 0
-    temp1 = []
-    for row in grid:
-        for column in grid:
-            count = count + 1
-            index2 = index2 + 1
-            if count == 6 or count == 12 or count == 18:
-                index2 = 0
-                index1 = index1 + 1
-            elif count == 24:
-                perimeter = 2 * (sum(temp1) + 1)
-                return perimeter
-            if grid[index1][index2] == 1:
-                temp1.append(grid[index1][index2])
+    perimeter = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column] == 1:
+                try:
+                    if grid[row - 1][column] == 0 or row == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+                
+                try:
+                    if grid[row + 1][column] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+
+                try:
+                    if grid[row][column - 1] == 0 or column == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+
+                try:
+                    if grid[row][column + 1] == 0:
+                        perimeter += 1
+                except IndexError:
+                    perimeter += 1
+    return perimeter
