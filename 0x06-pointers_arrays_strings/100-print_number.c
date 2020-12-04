98880622744cd7f4ba624cd4f2f58ab7b6fff5ce
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-04 11:42:16 -0500

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -7,17 +7,18 @@
 void print_number(int n)
 {
 	unsigned int num = n;
-
+	/* If number is smaller than 0, put a - sign */
+	/* and change number to positive */
 	if (n < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 
-	// Remove the last digit and recur
+	/* Remove the last digit and recur */
 	if (num / 10 != 0)
 		print_number(num / 10);
 
-	// Print the last digit
+	/* Print the last digit */
 	_putchar(num % 10 + '0');
 }
