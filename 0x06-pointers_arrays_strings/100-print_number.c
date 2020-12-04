56fdb04c4986289c9dfa7b70f78de940d13187ad
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-04 11:39:26 -0500

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * print_number - function that prints an integer.
@@ -9,25 +8,16 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	// Remove the last digit and recur
 	if (num / 10 != 0)
-	{
 		print_number(num / 10);
-		if (num > 0)
-		{
-			putchar(num % 10 + '0');
-		}
-		else
-		{
-			putchar(-num % 10 + '0');
-		}
-	}
-	else if ((num / 10 == 0) && (num % 10 != 0) && (num > 0))
-	{
-		putchar(num % 10 + '0');
-	}
-	else if ((num / 10 == 0) && (num % 10 != 0) && (num <= 0))
-	{
-		putchar('-');
-		putchar(-num % 10 + '0');
-	}
+
+	// Print the last digit
+	_putchar(num % 10 + '0');
 }
