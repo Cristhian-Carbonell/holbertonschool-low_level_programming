6445e66008f1d0fb4fbeca11e5f2ab7ebd3b1274
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-12-16 04:44:46 +0000

    alex

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,12 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[a] != '\0'; a++)
-	{
-		;
-	}
+	a = _strlen(s);
 	a--;
-	for (; a >= 0; a--)
+	while (a >= 0)
 	{
 		_putchar(s[a]);
+		a--;
 	}
 	_putchar('\n');
 }
