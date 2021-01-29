94bba50b0619619cf98cd0c9635a3917bb77b101
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-29 21:07:59 +0000

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,8 +18,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	
 	if (!ptr)
 	{
-		if(new_size < old_size)
-			new_size = old_size - new_size;
+		free(ptr);
 		ptr = malloc(new_size);
 		if (!ptr)
 			return (NULL);
