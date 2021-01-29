31cccbd1154bd9ff6c14e37a534c30ae8d3bc3fe
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-29 21:28:01 +0000

    update _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,6 +5,8 @@
  * @ptr: is a pointer to the memory previously allocated with a call to malloc
  * @old_size: is the size, in bytes, of the allocated space fot ptr
  * @new_size: is the new size, in bytes of the new memory block
+ *
+ * Return: new pointer or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
