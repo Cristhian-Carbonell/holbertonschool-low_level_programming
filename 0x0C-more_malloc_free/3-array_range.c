3464dcd4c1101cf553b2f656afeb9833c111b3a6
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2021-01-29 16:48:49 +0000

    update array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,13 +12,10 @@ int *array_range(int min, int max)
 	int *ptr, i, j;
 
 	if (min > max)
-	{
-		printf("ingreso aqui\n");
 		return (NULL);
-	}
 
 	ptr = (int *) malloc((max - min + 1) * sizeof(int));
-	if (ptr)
+	if (!ptr)
 		return (NULL);
 	for (j = min, i = 0; j <= max; i++, j++)
 		ptr[i] = j;
